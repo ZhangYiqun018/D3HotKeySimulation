3a685d3a89201f37f70ef5e61729e858a89a4edb
Yiqun Zhang <875977494@qq.com>
2022-10-04 22:26:11 +0800

    添加argparser

--- d3.py
+++ d3.py
@@ -73,6 +73,7 @@ if __name__ == '__main__':
     ## 1 按住不动
     ## 2 间隔按键
     ## 3 按住shift
+    ## 4 禁止
     config = get_args()
     if config.play_role == 'dh':
         from config.dh_config import *
